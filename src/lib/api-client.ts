2701566d6d0efcd73b959e61354cf0848ce699f5
AliArefnia <maarefnia1377@gmail.com>
2026-08-12 02:44:39 +0330

    mod: set fallback base url

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://api.qabile.com",
   headers: { "Content-Type": "application/json" },
 });
 
